7726553469c100a2ecc0be7a09c35a0f0780b9b7
2peralta <jamil.osoria.peralta@uni-hamburg.de>
2025-12-01 18:44:12 +0100

    Add baseline model with conv1d and dropout

--- dcase/src/models.py
+++ dcase/src/models.py
@@ -1,6 +1,6 @@
 import torch
+from torch import nn
 from torchaudio.transforms import MelSpectrogram
-from typing import Literal, List
 
 
 class BaselineModel(torch.nn.Module):
@@ -24,11 +24,24 @@ class BaselineModel(torch.nn.Module):
 
         # mel spectrogram as feature transform
         self.mel_transform = MelSpectrogram(
-            ???
+            sample_rate=sample_rate,
+            n_fft=n_fft,
+            f_min=f_min,
+            f_max=f_max,
+            n_mels=n_mels
         )
 
         # nn architecture
-        self.network = ???
+        layers = [nn.Conv1d(n_mels, n_hidden_feats, kernel_size=1), nn.ReLU(), nn.Dropout(dropout)]
+
+        for _ in range(n_hidden_layer - 1):
+            layers.append(nn.Conv1d(n_hidden_feats, n_hidden_feats, kernel_size=1))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(dropout))
+
+        layers.append(nn.Conv1d(n_hidden_feats, n_label, kernel_size=1))
+
+        self.network = nn.Sequential(*layers)
 
     def forward(
             self, 
@@ -37,14 +50,12 @@ class BaselineModel(torch.nn.Module):
         assert audio_data.shape[1] == 1, ('baseline is single-channel', audio_data.shape[1])
 
         # compute log-mel spectrogram as feature transform
-        features = ???  # (BATCH, FEAT, FRAMES)
+        features = torch.log(self.mel_transform(audio_data) + 1e-6)  # (BATCH, FEAT, FRAMES)
 
         # aggregate features
-        aggregated_features = ???  # (BATCH, FEAT, FRAMES')
+        aggregated_features = features.squeeze(1) # (BATCH, FEAT, FRAMES')
         
         # forward network
-        logits = ???  # (BATCH, FRAMES', LABEL)
+        logits = self.network(aggregated_features).transpose(1, 2)  # (BATCH, FRAMES', LABEL)
         
         return logits
-
-
